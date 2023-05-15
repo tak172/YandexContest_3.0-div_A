49ac42d62e0e48a3963ff54a26e86342bb45b44d
tak172 <timofeykromach172@gmail.com>
2023-05-16 00:10:41 +0300

    +Rase dir's

---
+++ Race/36/Solution.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
+int main() {
+	vector<int> sim(94, 0);
+	string str, ans, text;
+	fstream file("input.txt");
+	while(!file.eof()){ 
+		getline(file, text);
+		str += text;
+	}
+	int max = 0;
+	for (int i = 0; i < str.size(); i++) if ((int)str[i] >= 33 && (int)str[i] <= 126) {
+		sim[(int)str[i] - 33]++;
+		if (max < sim[(int)str[i] - 33]) max = sim[(int)str[i] - 33];
+	}
+	for (int i = 0; i < sim.size(); i++) if (sim[i]) ans += char(i + 33);
+	for (int i = 0; i < max; i++) {
+		for (int j = 0; j < sim.size(); j++)
+			if (sim[j]) if (sim[j] == max - i) {
+				cout << '#';
+				sim[j]--;
+			}
+			else cout << " ";
+		cout << endl;
+	}
+	cout << ans;
+	return 0;
+}
