313d83914277b381ea3e3fb350cdfa8951999e80
Timofey Kromachev <120005445+tak172@users.noreply.github.com>
2023-08-03 01:43:42 +0300

    Update Solution.cpp

--- Warm-up/8.Minimum rectangle/Solution.cpp
+++ Warm-up/8.Minimum rectangle/Solution.cpp
@@ -3,14 +3,18 @@
 using namespace std;
 const int INF = 10e10;
 int main() {
+	
 	struct toch {
 		int x;
 		int y;
 	};
-	int k; cin >> k;
+	
+	int k; 
+	cin >> k;
 	vector<toch> val(k);
 	toch max, min;
 	max.x = 0; max.y = 0; min.x = INF; min.y = INF;
+	
 	for (auto& it : val) {
 		cin >> it.x >> it.y;
 		if (max.x < it.x) max.x = it.x;
@@ -18,6 +22,8 @@ int main() {
 		if (min.x > it.x) min.x = it.x;
 		if (min.y > it.y) min.y = it.y;
 	}
+	
 	cout << min.x << " " << min.y << " " << max.x << " " << max.y;
 	return 0;
+	
 }
