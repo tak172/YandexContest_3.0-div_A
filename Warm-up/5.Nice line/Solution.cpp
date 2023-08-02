59ebefa93252da66351756d907acb031fe750d63
Timofey Kromachev <120005445+tak172@users.noreply.github.com>
2023-08-03 01:41:18 +0300

    Update Solution.cpp

--- Warm-up/5.Nice line/Solution.cpp
+++ Warm-up/5.Nice line/Solution.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
 long long min(long long a, long long b) {
 	if (a < b) return a;
 	return b;
 }
+
 int main() {
-	long long n, ans = 0; cin >> n;
+	
+	long long n, ans = 0; 
+	cin >> n;
 	vector<long long> val(n);
-	for (auto &it : val) cin >> it;
-	for (int i = 0; i < (int)val.size() - 1; i++) ans += min(val[i], val[i + 1]);
+	
+	for (auto &it : val) {
+		cin >> it;
+	}
+	
+	for (int i = 0; i < (int)val.size() - 1; i++){
+		ans += min(val[i], val[i + 1]);
+	}
+	
 	cout << ans;
 	return 0;
+	
 }
