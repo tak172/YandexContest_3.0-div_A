9acfe82106408ccd0da2af403d9b81abef699df6
Timofey Kromachev <120005445+tak172@users.noreply.github.com>
2023-08-03 01:44:22 +0300

    Update Solution.cpp

--- Warm-up/9.The sum in the rectangle/Solution.cpp
+++ Warm-up/9.The sum in the rectangle/Solution.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
 int main() {
+	
 	int m, n, k, x1, y1, x2, y2;
 	cin >> m >> n >> k;
 	vector<vector<int>> v(m + 1, vector<int>(n + 1, 0));
-	for (int i = 1; i <= m; i++)
+	
+	for (int i = 1; i <= m; i++) {
 		for (int j = 1; j <= n; j++) {
 			cin >> v[i][j];
 			v[i][j] += v[i - 1][j] + v[i][j - 1] - v[i - 1][j - 1];
 		}
+	}
+	
 	while (k--) {
 		cin >> x1 >> y1 >> x2 >> y2;
 		cout << v[x2][y2] - v[x1 - 1][y2] - v[x2][y1 - 1] + v[x1 - 1][y1 - 1] << endl;
 	}
+	
 	return 0;
+	
 }
