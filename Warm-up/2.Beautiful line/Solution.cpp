6e71842e547fc24f006c2afddfb3f8946785da65
Timofey Kromachev <120005445+tak172@users.noreply.github.com>
2023-08-03 01:38:07 +0300

    Update Solution.cpp

--- Warm-up/2.Beautiful line/Solution.cpp
+++ Warm-up/2.Beautiful line/Solution.cpp
@@ -3,30 +3,60 @@
 #include <string>
 using namespace std;
 int main() {
-	int k, ans = 0; cin >> k;
-	string s; cin >> s;
+	int k, ans = 0;
+	cin >> k;
+	
+	string s; 
+	cin >> s;
+	
 	map<char, int>  dic;
-	for (char i = 'a'; i <= 'z'; i++) dic[i] = 0;
+	
+	for (char i = 'a'; i <= 'z'; i++) {
+		dic[i] = 0;
+	}
+	
 	char rm = 'a';
+	
 	auto L = s.begin(), R = s.begin();
 
 	while (R != s.end()) {
-		for (auto it : dic) if (it.second > dic[rm]) rm = it.first;
+		for (auto it : dic) {
+			if (it.second > dic[rm]) {
+				rm = it.first;
+			}
+		}
+		
 		while (R != s.end() && R-L - dic[rm] <= k) {
 			dic[*R]++;
-			if (dic[*R] >= dic[rm]) rm = *R;
+			
+			if (dic[*R] >= dic[rm]) {
+				rm = *R;
+			}
+			
 			R++;
 		}
 
-		if (R - L - dic[rm] <= k && R - L > ans) ans = R - L;
-		else if (R - L - dic[rm] > k && R - L - 1 > ans) ans = R - L - 1;
+		if (R - L - dic[rm] <= k && R - L > ans) {
+			ans = R - L;
+		}
+		else if (R - L - dic[rm] > k && R - L - 1 > ans) {
+			ans = R - L - 1;
+		}
 
 		while (R - L - dic[rm] != k) {
 			dic[*L]--;
-			for (auto it : dic) if (it.second > dic[rm]) rm = it.first;
+			
+			for (auto it : dic) {
+				if (it.second > dic[rm]) {
+					rm = it.first;
+				}
+			}
+			
 			L++;
 		}
 	}
+	
 	cout << ans;
 	return 0;
+	
 }
