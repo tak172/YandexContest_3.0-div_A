0a0151e813279a691db8079081ad745dd3d39c7c
Timofey Kromachev <120005445+tak172@users.noreply.github.com>
2023-08-03 01:39:20 +0300

    Update Solution.cpp

--- Warm-up/3.Collector Diego/Solution.cpp
+++ Warm-up/3.Collector Diego/Solution.cpp
@@ -4,29 +4,42 @@
 #include <map>
 using namespace std;
 int main() {
-	int n, k, val = 0; cin >> n;
+	
+	int n, k, val = 0;
+	cin >> n;
+	
 	set<int> Di;
 	for (int i = 0; i < n; i++) {
 		cin >> k;
 		Di.insert(k);
 	}
+	
 	cin >> k;
 	vector<int> Col(k);
 	map<int, int> mCol;
-	for (int i = 0; i < k; i++){
+	
+	for (int i = 0; i < k; i++) {
 		cin >> Col[i];
 		mCol[Col[i]] = 0;
 	}
+	
 	auto itm = mCol.begin();
 	auto its = Di.begin();
-	for (; itm != mCol.end(); itm++){
+	
+	for (; itm != mCol.end(); itm++) {
+		
 		while (its != Di.end() && *its < itm->first) {
 			itm->second++;
 			its++;
 		}
+		
 		itm->second += val;
 		val = itm->second;
 	}
-	for (auto it : Col) cout << mCol[it] << endl;
+	
+	for (auto it : Col)
+		cout << mCol[it] << endl;
+	
 	return 0;
+	
 }
