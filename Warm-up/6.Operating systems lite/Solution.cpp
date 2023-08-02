fc4bcb0700fe40908a17d071bcb654216a7244bf
Timofey Kromachev <120005445+tak172@users.noreply.github.com>
2023-08-03 01:42:07 +0300

    Update Solution.cpp

--- Warm-up/6.Operating systems lite/Solution.cpp
+++ Warm-up/6.Operating systems lite/Solution.cpp
@@ -9,17 +9,27 @@ struct val {
 };
 
 int main() {
+	
 	int m, n, ans = 0;
 	cin >> m >> n;
 	vector<val> x(n);
-	for (int i = 0; i < n; i++) cin >> x[i].l >> x[i].r;
+	
+	for (int i = 0; i < n; i++) {
+		cin >> x[i].l >> x[i].r;
+	}
 
-	for (int i = n - 1; i > 0; i--)
+	for (int i = n - 1; i > 0; i--) {
 		for (int j = 0; j <= i-1; j++) {
-			if (!(x[i].l > x[j].r || x[i].r < x[j].l)) x[j].fl = false;
+			if (!(x[i].l > x[j].r || x[i].r < x[j].l))
+				x[j].fl = false;
 		}
+	}
 
-	for (auto &it : x) if (it.fl) ans++;
+	for (auto &it : x) {
+		if (it.fl) ans++;
+	}
+	
 	cout << ans;
 	return 0;
+	
 }
