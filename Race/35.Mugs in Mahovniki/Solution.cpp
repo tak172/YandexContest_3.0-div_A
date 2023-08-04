8b8a611cc1ae8e1d1b790d93380ab8014b8d267d
Timofey Kromachev <120005445+tak172@users.noreply.github.com>
2023-08-04 17:28:09 +0300

    Update Solution.cpp

--- Race/35.Mugs in Mahovniki/Solution.cpp
+++ Race/35.Mugs in Mahovniki/Solution.cpp
@@ -2,31 +2,46 @@
 #include<vector>
 #include<set>
 using namespace std;
+
 int main() {
-	int n; cin >> n;
+	
+	int n;
+	cin >> n;
 	vector<vector<int>> r(n + 1);
 	vector<int> c(n + 1, 0), ans;
 	set<int> emp;
 	int val, to;
+	
 	for (int i = 1; i < r.size(); i++) {
 		cin >> val;
+		
 		for (int j = 0; j < val; j++) {
 			cin >> to;
 			r[i].push_back(to);
 			c[to]++;
 		}
+		
 	}
-	for (int i = 1; i < c.size(); i++) if (c[i] == 0) emp.insert(i);
+	
+	for (int i = 1; i < c.size(); i++)
+		if (c[i] == 0) 
+			emp.insert(i);
+	
 	while (!emp.empty()) {
 		val = *emp.rbegin();
 		emp.erase(*emp.rbegin());
 		ans.push_back(val);
+		
 		for (int i = 0; i < r[val].size(); i++) {
 			to = r[val][i];
 			c[to]--;
 			if (c[to] == 0) emp.insert(to);
 		}
+		
 	}
-	for (int i = ans.size() - 1; i >= 0; i--) cout << ans[i] << " ";
+	for (int i = ans.size() - 1; i >= 0; i--)
+		cout << ans[i] << " ";
+	
 	return 0;
+	
 }
