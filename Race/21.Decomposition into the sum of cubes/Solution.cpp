a5d2027ccf48904f870a063de7b9c488b9edb842
Timofey Kromachev <120005445+tak172@users.noreply.github.com>
2023-08-04 17:03:18 +0300

    Update Solution.cpp

--- Race/21.Decomposition into the sum of cubes/Solution.cpp
+++ Race/21.Decomposition into the sum of cubes/Solution.cpp
@@ -2,17 +2,32 @@
 #include <vector>
 #include <cmath>
 using namespace std;
+
 int main() {
-	int n; cin >> n;
+	
+	int n; 
+	cin >> n;
+	
 	vector<int> p(101);
-	for (int i = 0; i < p.size(); i++) p[i] = pow(i, 3);
+	
+	for (int i = 0; i < p.size(); i++)
+		p[i] = pow(i, 3);
+	
 	vector<int> a(n+1, (int)10e6);
 	a[0] = 0;
 	a[1] = 1;
-	for (int i = 2; i < n + 1; i++)
-		for (int j = 1; p[j] <= i; j++)
-			if (p[j] <= i) a[i] = min(1 + a[i - p[j]], a[i]);
-			else break;
+	
+	for (int i = 2; i < n + 1; i++) {
+		for (int j = 1; p[j] <= i; j++) {
+			if (p[j] <= i)
+				a[i] = min(1 + a[i - p[j]], a[i]);
+			else
+				break;
+		}
+	}
+	
 	cout << a[n];
+	
 	return 0;
+	
 }
