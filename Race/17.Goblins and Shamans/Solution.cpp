d6a0fecaff4d317e85a49765079707e6c3510389
Timofey Kromachev <120005445+tak172@users.noreply.github.com>
2023-08-04 16:57:16 +0300

    Update Solution.cpp

--- Race/17.Goblins and Shamans/Solution.cpp
+++ Race/17.Goblins and Shamans/Solution.cpp
@@ -2,38 +2,49 @@
 #include <list>
 using namespace std;
 int main() {
-	int n, num; cin >> n;
+	
+	int n, num;
+	cin >> n;
 	list<int> f, s;
 	char act;
+	
 	while (n--) {
 		cin >> act;
+		
 		if (s.size() > f.size()) {
 			while (s.size() > f.size()) {
 				f.push_back(s.front());
 				s.pop_front();
 			}
 		}
+		
 		if (s.size() < f.size()) {
 			while (s.size() + 1 < f.size()) {
 				s.push_front(f.back());
 				f.pop_back();
 			}
 		}
+		
 		if (act == '+') {
 			cin >> num;
 			s.push_back(num);
 			continue;
 		}
+		
 		if (act == '*') {
 			cin >> num;
 			s.push_front(num);
 			continue;
 		}
+		
 		if (act == '-') {
 			if (f.size() == 0) f.push_front(s.front());
 			cout << f.front() << endl;
 			f.pop_front();
 		}
+		
 	}
+	
 	return 0;
+	
 }
