c8665bd1d0089f9f3250ffa04751907677040625
Timofey Kromachev <120005445+tak172@users.noreply.github.com>
2023-08-04 16:50:48 +0300

    Update Solution.cpp

--- Race/14.Histogram and rectangle/Solution.cpp
+++ Race/14.Histogram and rectangle/Solution.cpp
@@ -1,29 +1,39 @@
 #include <iostream>
 #include <stack>
 using namespace std;
+
 long long max(long long a, long long b);
+
 struct row {
 	long long lenght;
 	long long count = 1;
 };
+
 int main() {
-	long long n, idx = 0; cin >> n;
+	
+	long long n, idx = 0; 
+	cin >> n;
 	stack<row> st;
 	row val;
 	long long ans = 0;
 	cin >> val.lenght;
 	st.push(val);
 	val.count = 1;
-	for (int i = 1; i < n; i++){
+	
+	for (int i = 1; i < n; i++) {
 		cin >> val.lenght;
 		val.count = 1;
-		if (val.lenght > st.top().lenght) st.push(val);
+		
+		if (val.lenght > st.top().lenght) {
+			st.push(val);
+		}
 		else {
 			while (!st.empty() && val.lenght < st.top().lenght) {
 				idx += st.top().count;
 				ans = max(ans, st.top().lenght * idx);
 				st.pop();
 			}
+			
 			if (!st.empty() && st.top().lenght == val.lenght) {
 				val.count += st.top().count + idx;
 				st.pop();
@@ -33,19 +43,26 @@ int main() {
 			idx = 0;
 		}
 	}
-	if (st.empty()) cout << ans;
+	if (st.empty())
+		cout << ans;
 	else {
 		idx = 0;
+		
 		while (!st.empty()) {
 			idx+= st.top().count;
 			ans = max(ans, st.top().lenght * idx);
 			st.pop();
 		}
+		
 		cout << ans;
 	}
+	
 	return 0;
 }
+
 long long max(long long a, long long b) {
+	
 	if (a > b) return a;
 	return b;
+	
 }
