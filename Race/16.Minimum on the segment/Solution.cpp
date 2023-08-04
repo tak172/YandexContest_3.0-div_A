719043c14460143a7650b5fbaf8676fdc100b890
Timofey Kromachev <120005445+tak172@users.noreply.github.com>
2023-08-04 16:56:10 +0300

    Update Solution.cpp

--- Race/16.Minimum on the segment/Solution.cpp
+++ Race/16.Minimum on the segment/Solution.cpp
@@ -2,18 +2,33 @@
 #include <vector>
 #include <map>
 using namespace std;
+
 int main() {
+	
 	int n, k;
 	cin >> n >> k;
 	vector<int> a(n);
 	map<int, int> b;
-	for (int i = 0; i < n; i++) cin >> a[i];
-	for (int i = 0; i < k; i++) b[a[i]] = i;
-	for (int i = 0;; i++){
-		while((!b.empty()) && b.begin()->second < i) b.erase(b.find(b.begin()->first));
+	
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+	
+	for (int i = 0; i < k; i++)
+		b[a[i]] = i;
+	
+	for (int i = 0;; i++) {
+		
+		while((!b.empty()) && b.begin()->second < i)
+			b.erase(b.find(b.begin()->first));
+		
 		cout << b.begin()->first << endl;
-		if (i + k < n) b[a[i + k]] = i + k;
+		
+		if (i + k < n) 
+			b[a[i + k]] = i + k;
+			
 		else return 0;
+		
 	}
+	
 	return 0;
 }
