8a5b39279a549f0a5a3d7a98abe251ec4df71e67
Timofey Kromachev <120005445+tak172@users.noreply.github.com>
2023-08-04 17:02:20 +0300

    Update Solution.cpp

--- Race/20.Machines/Solution.cpp
+++ Race/20.Machines/Solution.cpp
@@ -4,8 +4,11 @@
 #include<vector>
 #include<fstream>
 #include<map>
+
 #define INF 500001
+
 using namespace std;
+
 struct comp
 {
     template<typename T>
@@ -18,20 +21,26 @@ struct comp
         return l.first < r.first;
     }
 };
+
 int main() {
     int n, k, p, ans = 0;
     cin >> n >> k >> p;
+    
     vector<int> d(p);
     vector<list<int>> x(n + 1);
+    
     for (int i = 0; i < p; i++) {
         cin >> d[i];
         x[d[i]].push_back(i);
     }
+    
     set<int> floor;
     set<pair<int, int>, comp> my_map;
     int i = 0;
+    
     for (; floor.size() != k; i++) {
         x[d[i]].pop_front();
+        
         if (x[d[i]].size() == 0) {
             pair<int, int> t = make_pair(d[i], INF);
             my_map.insert(t);
@@ -40,12 +49,17 @@ int main() {
             pair<int, int> t = make_pair(d[i], *x[d[i]].begin());
             my_map.insert(t);
         }
+        
         floor.insert(d[i]);
     }
+    
     ans += k;
+    
     for (; i < p; i++) {
+        
         if (floor.find(d[i]) != floor.end()) {
             x[d[i]].pop_front();
+            
             if (x[d[i]].size() == 0) {
                 pair<int, int> t = make_pair(d[i], INF);
                 my_map.insert(t);
@@ -54,12 +68,15 @@ int main() {
                 pair<int, int> t = make_pair(d[i], *x[d[i]].begin());
                 my_map.insert(t);
             }
+            
         }
         else {
+            
             floor.erase(my_map.rbegin()->first);
             my_map.erase(--my_map.end());
             floor.insert(d[i]);
             x[d[i]].pop_front();
+            
             if (x[d[i]].size() == 0) {
                 pair<int, int> t = make_pair(d[i], INF);
                 my_map.insert(t);
@@ -68,9 +85,13 @@ int main() {
                 pair<int, int> t = make_pair(d[i], *x[d[i]].begin());
                 my_map.insert(t);
             }
+            
             ans++;
         }
     }
+    
     cout << ans;
+    
     return 0;
+    
 }
