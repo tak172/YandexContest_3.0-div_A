c0b8bd405b37d2c1169f1986d6c490b30a92e0eb
Timofey Kromachev <120005445+tak172@users.noreply.github.com>
2023-08-04 17:17:03 +0300

    Update Solution.cpp

--- Race/28.Space scavenger/Solution.cpp
+++ Race/28.Space scavenger/Solution.cpp
@@ -2,28 +2,50 @@
 #include <vector>
 #include <string>
 using namespace std;
+
 long long Find(char x, vector<char>& dic);
 long long Foo(long long& s, vector<char>& dic, string& l, long long i, vector<vector<long long>>& d);
 
 int main() {
+	
 	vector<char> dic{ 'N', 'S', 'W', 'E', 'U', 'D' };
 	vector<string> l(6);
-	for (auto& it : l) getline(cin, it);
+	
+	for (auto& it : l) 
+		getline(cin, it);
+	
 	char sim;
 	long long n, s; cin >> sim >> n;
 	vector<vector<long long>> d(n + 1, vector<long long>(6, 0));
+	
 	for (long long i = 1; i < n + 1; i++) {
 		for (long long j = 0; j < 6; j++) {
 			s = Foo(s, dic, l[j], i, d);
 			d[i][j] = s + 1;
 		}
 	}
+	
 	cout << d[n][Find(sim, dic)];
+	
 	return 0;
+	
 }
+
 long long Foo(long long& s, vector<char>& dic, string& l, long long i, vector<vector<long long>>& d) {
+	
 	s = 0;
-	for (long long k = 0; k < l.size(); k++) s += d[i - 1][Find(l[k], dic)];
+	
+	for (long long k = 0; k < l.size(); k++)
+		s += d[i - 1][Find(l[k], dic)];
+	
 	return s;
+	
 }
-long long Find(char x, vector<char>& dic) { for (int i = 0; i < dic.size(); i++) if (x == dic[i]) return i; }
+
+long long Find(char x, vector<char>& dic) {
+	
+	for (int i = 0; i < dic.size(); i++) 
+		if (x == dic[i])
+			return i; 
+	
+}
