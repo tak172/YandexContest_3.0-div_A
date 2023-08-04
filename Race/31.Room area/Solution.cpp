5829089271b8b85536e7d478495472b249649b6a
Timofey Kromachev <120005445+tak172@users.noreply.github.com>
2023-08-04 17:20:49 +0300

    Update Solution.cpp

--- Race/31.Room area/Solution.cpp
+++ Race/31.Room area/Solution.cpp
@@ -2,22 +2,34 @@
 #include <vector>
 #include <string>
 using namespace std;
+
 int Count = 0;
 void Rec(vector<vector<char>>& r, int i, int j);
+
 int main() {
-	int n, coord_i, coord_j; cin >> n;
+	
+	int n, coord_i, coord_j; 
+	cin >> n;
 	vector<vector<char>> r(n, vector<char>(n));
 	string str;
+	
 	for (int i = 0; i < n; i++) {
 		cin >> str;
-		for (int j = 0; j < n; j++) r[i][j] = str[j];
+		
+		for (int j = 0; j < n; j++) 
+			r[i][j] = str[j];
+		
 	}
+	
 	cin >> coord_i >> coord_j;
 	Rec(r, coord_i - 1, coord_j - 1);
 	cout << Count;
+	
 	return 0;
+	
 }
 void Rec(vector<vector<char>>& r, int i, int j) {
+	
 	if (r[i][j] == '.') {
 		Count++;
 		r[i][j] = '*';
@@ -26,4 +38,5 @@ void Rec(vector<vector<char>>& r, int i, int j) {
 		Rec(r, i + 1, j);
 		Rec(r, i, j + 1);
 	}
+	
 }
