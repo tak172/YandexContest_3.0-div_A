f2a696452b8adf82b50651ead27d6b4f38371051
Timofey Kromachev <120005445+tak172@users.noreply.github.com>
2023-08-04 17:15:41 +0300

    Update Solution.cpp

--- Race/27.Levenstein distance/Solution.cpp
+++ Race/27.Levenstein distance/Solution.cpp
@@ -5,18 +5,32 @@
 using namespace std;
 
 int main() {
-	string str1, str2; cin >> str1 >> str2;
+	
+	string str1, str2; 
+	cin >> str1 >> str2;
+	
 	int n = (int)str1.size(), m = (int)str2.size();
 	vector<vector<int>> d(n + 1, vector<int>(m + 1, 0));
-	for (int i = 0; i < n + 1; i++)	d[i][0] = i;
-	for (int i = 0; i < m + 1; i++)	d[0][i] = i;
+	
+	for (int i = 0; i < n + 1; i++)	
+		d[i][0] = i;
+	
+	for (int i = 0; i < m + 1; i++)
+		d[0][i] = i;
+	
 	for (int i = 1; i < n + 1; i++) {
 		for (int j = 1; j < m + 1; j++) {
+			
 			d[i][j] = min(d[i][j - 1], d[i - 1][j]) + 1;
-			if (str1[i - 1] == str2[j - 1]) d[i][j] = min(d[i][j], d[i - 1][j - 1]);
-			else d[i][j] = min(d[i][j], d[i - 1][j - 1] + 1);
+			if (str1[i - 1] == str2[j - 1])
+				d[i][j] = min(d[i][j], d[i - 1][j - 1]);
+			else 
+				d[i][j] = min(d[i][j], d[i - 1][j - 1] + 1);
 		}
 	}
+	
 	cout << d[n][m];
+	
 	return 0;
+	
 }
