f416dec4d30ea2cd4d4f552e797088baf875545f
Timofey Kromachev <120005445+tak172@users.noreply.github.com>
2023-08-04 17:00:49 +0300

    Update Solution.cpp

--- Race/19.Commercial calculator/Solution.cpp
+++ Race/19.Commercial calculator/Solution.cpp
@@ -4,24 +4,34 @@
 using namespace std;
 
 int main() {
+	
 	multiset<double> s;
-	int n; cin >> n;
+	int n; 
+	cin >> n;
 	double val, temp;
+	
 	for (int i = 0; i < n; i++) {
 		cin >> val;
 		s.insert(val);
 	}
+	
 	val = 0;
+	
 	while (s.size() != 1) {
 		temp = *s.begin();
 		s.erase(s.begin());
+		
 		temp += *s.begin();
 		s.erase(s.begin());
+		
 		val += temp * 0.05;
 		s.insert(temp);
 	}
+	
 	cout.setf(ios::fixed);
 	cout.precision(2);
 	cout << val;
+	
 	return 0;
+	
 }
